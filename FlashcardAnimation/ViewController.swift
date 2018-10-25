2d74a3ecac770d8eb8e946cde637824139bd6278
vendredi <ts471@sbcglobal.net>
2018-10-25 09:39:03 -0400

    Initial Commit

---
+++ FlashcardAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashcardAnimation
+//
+//  Created by Ted Sauerbrey on 10/25/18.
+//  Copyright © 2018 Ted Sauerbrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
